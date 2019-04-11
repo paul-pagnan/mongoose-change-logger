57e48891ab4706c1224b65434cbf573e65512ce3
Paul Pagnan <paul@pagnan.com.au>
2019-04-11 10:14:41 +1000

    [ci skip] Updates

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,3 @@
 var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"mongooseChangeLogger","url":"globals.html#mongoosechangelogger","classes":"tsd-kind-function"}]};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"init","url":"globals.html#init","classes":"tsd-kind-function tsd-is-not-exported"},{"id":1,"kind":64,"name":"mongooseChangeLogger","url":"globals.html#mongoosechangelogger","classes":"tsd-kind-function"}]};
