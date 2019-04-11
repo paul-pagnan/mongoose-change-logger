16729e7eb3add085becc83c0be8d5c08b4cca84c
Paul Pagnan <paul@pagnan.com.au>
2019-04-11 13:46:34 +1000

    [ci skip] Updates

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,3 @@
 var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"init","url":"globals.html#init","classes":"tsd-kind-function tsd-is-not-exported"},{"id":1,"kind":64,"name":"mongooseChangeLogger","url":"globals.html#mongoosechangelogger","classes":"tsd-kind-function"}]};
+            typedoc.search.data = {"kinds":{"32":"Variable","64":"Function"},"rows":[{"id":0,"kind":32,"name":"client","url":"globals.html#client","classes":"tsd-kind-variable tsd-is-not-exported"},{"id":1,"kind":64,"name":"init","url":"globals.html#init","classes":"tsd-kind-function tsd-is-not-exported"},{"id":2,"kind":64,"name":"mongooseChangeLogger","url":"globals.html#mongoosechangelogger","classes":"tsd-kind-function"}]};
